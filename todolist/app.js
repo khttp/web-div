a841c45e48cf7352a9e76538847a29845530ee4b
abooh <abdelrahmankhattab9999@gmail.com>
2022-10-09 03:08:26 +0200

    update the app with mongoose now we use mongodb

--- todolist/app.js
+++ todolist/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const {urlencoded} = require('body-parser');
 const date = require(__dirname+'/date.js');
+const mongoose = require('mongoose');
 
 app=express();
 app.use(urlencoded({extended:true}));
@@ -10,32 +11,58 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 app.use(express.static(__dirname));
 
-items=['buy food','cook food','eat food'];
-workItems=[];
+//connect to a database
+mongoose.connect('mongodb://localhost:27017/todolistDB');
+
+const todoSchema=new mongoose.Schema({
+    name:String
+})
+const todoList= mongoose.model('todoList',todoSchema)
+
+const task1=new todoList({
+    name:'buy food'
+})
+const task2=new todoList({
+    name:'cook food'
+})
+const task3=new todoList({
+    name:'eat food'
+})
+
+
 app.get('/',(req,res)=>{
-res.render('index',{listName:date.getDate(),listItems:items})
+    
+    todoList.find({},(err,tasks)=>{
+        if(tasks.length===0){
+            todoList.insertMany([task1,task2,task3])
+  
+            res.redirect('/')
+            }else{
+            res.render('index',{listName:'Today!!!',listItems:tasks})
+    }
+});
 })
 
 
 app.post('/',(req,res)=>{
-    if (req.body.list=='work'){
-        workItems.push(req.body.newItem)
-        res.redirect('/work')
-    }else{
-    items.push(req.body.newItem)
+    const addItem=new todoList({
+        name:req.body.newItem
+    })
+    addItem.save()
+     
     res.redirect('/')
-    }
+});
+app.post('/delete',(req,res)=>{
+   console.log(req.body._id);
+    todoList.findByIdAndDelete(req.body._id,(err)=>{
+        if (!err){
+            
+            console.log('deleted');
+            res.redirect('/');
+        }
+    })
 })
 
-app.get('/work',(req,res)=>{
-    
-    res.render('index',{listName:'work list',listItems:workItems});
-})
-
-app.post('/work',(req,res)=>{
-    workItems.push(req.body.newItem) 
-    res.redirect('/work')
-})
 
 
 app.listen(3000,()=>{
