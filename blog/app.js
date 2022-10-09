f7efb0f03f1def3008fb1c6186d803a512cb4d86
abooh <abdelrahmankhattab9999@gmail.com>
2022-10-09 18:53:37 +0200

    using mongoose to store the posts of the user

--- blog/app.js
+++ blog/app.js
@@ -4,10 +4,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const {urlencoded} = require("body-parser");
-
+const mongoose = require('mongoose');
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+mongoose.connect('mongodb://localhost:27017/blogDB');
+
+blogSchema={
+    title:String,
+    content:String
+}
+
+const blog=new mongoose.model('blog',blogSchema);
+
 
 const app = express();
 let posts=[]
@@ -16,7 +26,12 @@ app.use(urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get('/',(req,res)=>{
-    res.render('home',{paragraph:homeStartingContent,comments:posts});
+    blog.find((err,foundPosts)=>{
+        if(!err){
+
+    res.render('home',{paragraph:homeStartingContent,comments:foundPosts});
+        }
+    })
 
 });
 app.get('/about',(req,res)=>{
@@ -29,27 +44,27 @@ app.get('/compose',(req,res)=>{
     res.render('compose') 
 });
 app.post('/compose',(req,res)=>{
-    let post={
-        'title':req.body.postTitle,
-        'body':req.body.postBody
-    }
-    posts.push(post);
+     const post=new blog({
+        title:req.body.postTitle,
+        content:req.body.postBody
+    })
+    post.save();
     res.redirect('/')
 })
 app.get('/posts/:postname',(req,res)=>{
-        
- posts.forEach((comment)=>{
-        
-        if (_.lowerCase(comment.title)=== _.lowerCase(req.params.postname)){
 
-                res.render('post',{header:comment.title,paragraph:comment.body.readmore()}) 
-        }
- })
+    blog.findOne({title:req.params.postname},(err,foundPost)=>{
+        if (!err) {
+
+                res.render('post',{header:foundPost.title,paragraph:foundPost.content}) 
+        }else{console.log(err);}
+    }) 
+        
         
     
     
 })
-app.listen(3000, function() {
+app.listen(3000, ()=> {
   console.log("Server started on port 3000");
 });
 
