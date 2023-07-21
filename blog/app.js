2fd2857ef1b241359fc846361f23681b946b2335
abooh <abdelrahmankhattab9999@gmail.com>
2023-07-21 15:32:35 +0300

    revisit the code and perform some updates

--- blog/app.js
+++ blog/app.js
@@ -10,7 +10,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 mongoose.connect('mongodb://localhost:27017/blogDB');
-
+mongoose.set('strictQuery', false);
 blogSchema={
     title:String,
     content:String
@@ -30,7 +30,7 @@ app.get('/',(req,res)=>{
         if(!err){
 
     res.render('home',{paragraph:homeStartingContent,comments:foundPosts});
-        }
+        }else throw err
     })
 
 });
