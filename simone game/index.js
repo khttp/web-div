f09df34b8a5447d13450cee9211ed5f0861aef77
abooh <abdelrahmankhattab9999@gmail.com>
2023-09-18 22:43:05 +0300

    refactoring the code and fixing simone game for the last time

--- simone game/index.js
+++ simone game/index.js
@@ -2,36 +2,47 @@
 let progress=1;
 let playerPattern=[];
 let game;
+let colorset = {1:"yellow",2:"green",3:"red",4:"blue"}
 
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-//change the level
-level=function(game){
-    
-    if (game=== true){
-        $('h1').text('level '+progress+'!');
-        $('button').prop('disabled',false);
-        progress++;
-        main();
-        
-    }
-    else{
-        
-        $('h1').html('you lost </br>your level is '+progress+'!')
-    }
 
+
+// generate a random pattern for the user to repeat
+generatePattern=function() {
+let pattern=Array.from({length:progress},()=>
+    Math.floor(Math.random()*4+1)   
+)
+const dosomething=async()=>{
+for (let color of pattern){
+    console.log(colorset[color])
+
+    
+        tune=new Audio('sounds/'+colorset[color]+'.mp3')
+        tune.play();
+       $('.'+color).addClass('click')
+
+       await sleep(500)
+
+       $('.'+color).removeClass('click')
+       await sleep(500)
+    
+    }
+}
+dosomething();
+ return pattern;
 }
 
-// store player pattern and play the sound
 
+// store player pattern and play the sound
 storePattern=function(){
   
     let colorAndNumber=this.className.split(' ');
     
     //sound of a button
-    tune=new Audio('/sounds/'+colorAndNumber[0]+'.mp3')
+    tune=new Audio('sounds/'+colorAndNumber[0]+'.mp3')
     tune.play();
 
     console.log(colorAndNumber[1])
@@ -54,26 +65,22 @@ storePattern=function(){
 }
 
 
-// generate a random pattern for the user to repeat
-generatePattern=function() {
-let pattern=Array.from({length:progress},()=>
-    Math.floor(Math.random()*4+1)   
-)
-const dosomething=async()=>{
-for (let color of pattern){
-    console.log(color)
 
+//change the level
+level=function(game){
     
-       $('.'+color).addClass('click')
-       await sleep(500)
-
-       $('.'+color).removeClass('click')
-       await sleep(500)
-    
+    if (game=== true){
+        progress+=1;
+        $('h1').text('level '+progress+'!');
+        $('button').prop('disabled',false);
+        main();
+        
     }
-}
-dosomething()
- return pattern;
+    else{
+       progress-=1; 
+        $('h1').html('you lost </br>your level is '+progress+'!')
+    }
+
 }
 
 
@@ -88,3 +95,6 @@ function main(){
 }
 
 main();
+
+
+
